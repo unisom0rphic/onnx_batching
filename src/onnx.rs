562a4d2f2fb67c419fb6aaed7709920f577d06e1
unisom0rphic <117302827+unisom0rphic@users.noreply.github.com>
2026-05-08 00:18:55 +0500

    feat: finish batch_infer(), add sync::Once for model initialization

--- src/onnx.rs
+++ src/onnx.rs
@@ -3,16 +3,21 @@ use ort::{
     session::{Session, SessionOutputs},
     value::{Tensor, TensorRef},
 };
+use tokio::time::interval_at;
 
 pub struct OnnxModel {
     session: Session,
 }
 
+static ONNX_INIT: std::sync::Once = std::sync::Once::new();
+
 impl OnnxModel {
     // TODO: with_intra_threads for multithreading
     /// Loads specified ONNX model from PATH
     pub fn load_onnx(path: &str) -> ort::Result<Self> {
-        ort::init().commit();
+        ONNX_INIT.call_once(|| {
+            ort::init().commit();
+        });
 
         let session = Session::builder()?.commit_from_file(path)?;
 
@@ -21,17 +26,11 @@ impl OnnxModel {
 
     /// Runs batched inference for the provided session
     pub fn batch_infer(&mut self, input_batch: Vec<Vec<f32>>) -> ort::Result<Vec<Vec<f32>>> {
-        // should run batched inference for the input batch,
-        // which is either a 2D vector or NDarray
-
-        // outputs (self.session.run()) is SessionOutputs<'_>
-        // and outputs[i].try_extract_tensor() returns (&Shape, &[f32])
-
-        // https://docs.rs/ort/latest/ort/session/struct.SessionOutputs.html
-
-        // we can *probably* iterate through outputs though not sure,
-        // iirc it contains three fields which are keys, values and length?
+        // TODO: accept either Vec<Vec<f32>> or Array2<f32> (via enum) and pattern match
         let rows = input_batch.len();
+        if rows == 0 {
+            return Ok(vec![]);
+        }
         let columns = input_batch[0].len();
 
         let inputs = ndarray::Array2::<f32>::from_shape_vec(
@@ -40,15 +39,22 @@ impl OnnxModel {
         )
         .map_err(|e| ort::Error::new(e.to_string()))?;
 
-        let outputs = self
-            .session
-            .run(ort::inputs![TensorRef::from_array_view(&inputs)?])?;
-        for el in outputs {
-            // el is &str, Value??? what is that 😭😭😭
-        }
-        // let tensors = outputs[0].try_extract_tensor::<f32>()?;
-        // TODO: are &[f32] values?
+        let inputs = Tensor::from_array(inputs)?;
 
-        todo!("WIP");
+        // https://docs.rs/ort/latest/ort/session/struct.SessionOutputs.html
+        let outputs = self.session.run(ort::inputs!["input" => inputs])?;
+
+        // outputs is basically a hashmap {output: value} but as a tuple
+        let value = &outputs["output"];
+        let (shape, tensor_data) = value.try_extract_tensor::<f32>()?;
+
+        let out_cols = shape[1] as usize;
+
+        let result = tensor_data
+            .chunks_exact(out_cols)
+            .map(|chunk| chunk.to_vec())
+            .collect();
+
+        Ok(result)
     }
 }
